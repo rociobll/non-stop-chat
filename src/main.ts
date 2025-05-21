2d94bb84e681d7c5d3cff59a61455931269daeaf
Rocio <rocio.ballo27@gmail.com>
2025-05-21 15:11:31 +0200

    FIX: Corrijo angular.json que peso de estilos sea mayor daba error al compilar, y en main.ts añado hash strategy en providers para evitar errores de enrutamiento en Netlify

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 // Configuracion Firebase y Base datos:
 import { environment } from './environments/environment';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';  //conectar inicar app con firebase
-import { provideAuth, getAuth } from '@angular/fire/auth';  //servicios autenticación
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'; //conectar inicar app con firebase
+import { provideAuth, getAuth } from '@angular/fire/auth'; //servicios autenticación
 import { getDatabase, provideDatabase } from '@angular/fire/database'; //bbdd Realtime que es la que uso
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 //import { getFirestore, provideFirestore } from '@angular/fire/firestore'; //bbdd de firestore, si se usase
-
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -23,9 +30,13 @@ bootstrapApplication(AppComponent, {
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
-    provideHttpClient() // para bbdd Realtimedatabase la que usaré
-   // provideFirestore(() => getFirestore()), //para bbdd firebase
+    provideHttpClient(), // para bbdd Realtimedatabase la que usaré
+    // provideFirestore(() => getFirestore()), //para bbdd firebase
 
+    //Hash strategy - para que al recargar en netlify no de page not found
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
   ],
 }).catch((error) => console.log(error));
-
